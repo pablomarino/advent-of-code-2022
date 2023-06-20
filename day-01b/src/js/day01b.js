2c5eaa5a083223b7b161a72e1ed3092d3b7e7fe9
Pablo Mariño Boga <pmarino@icarto.es>
2023-06-20 22:55:39 +0200

    Modifies tests

--- day-01b/src/js/day01b.js
+++ day-01b/src/js/day01b.js
@@ -41,13 +41,4 @@ const calorie_counter = async(filePath) =>{
   return top_3_elves;
 }
 
-/*
-// Give the answer
-const filePath = 'input/input.txt';
-calorie_counter(filePath).then(result => { 
-  console.table(result);
-  let total_calories = result.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(`Top 3 Elves total calories: ${total_calories}`);
-});
-*/
 module.exports = calorie_counter;
--- day-01b/src/js/day01b.test.js
+++ day-01b/src/js/day01b.test.js
@@ -1,21 +1,23 @@
 const calorie_counter = require('./day01b.js');
 const filePath = 'input/input.txt';
 // Given the input file, 
-// When the top 3 elves should have the following calorie counts:
-
-test('then the returned value length should be 3', () => {
-  expect(calorie_counter(filePath).then(
-    result => {
-      expect(result.length).toBe(3);
-    }
-  ));
+// When thtop 3 elves should have the following calorie counts:
+describe('GIVEN the input.txt file WHEN calculates top 3 elves calorie count', () => {
+  test('THEN the returned value length should be 3', () => {
+    expect(calorie_counter(filePath).then(
+      result => {
+        expect(result.length).toBe(3);
+      }
+    ));
+  });
 });
 
-test('the sum of the top3 values should be 203203', () => {
-  expect(calorie_counter(filePath).then(
-    result => {
-      expect(result.reduce((accumulator, currentValue) => accumulator + currentValue, 0)).toBe(203203);
-    }
-  ));
+describe('GIVEN the input.txt file WHEN calculates top 3 elves calorie count', () => {
+  test('THEN the sum of the top3 values should be 203203', () => {
+    expect(calorie_counter(filePath).then(
+      result => {
+        expect(result.reduce((accumulator, currentValue) => accumulator + currentValue, 0)).toBe(203203);
+      }
+    ));
+  });
 });
-
