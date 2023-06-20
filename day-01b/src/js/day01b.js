f5081a8bc46440e4d2dc533e3c00ca75c8729337
Pablo Mariño Boga <pmarino@icarto.es>
2023-06-20 16:43:01 +0200

    Refactor day01b

--- day-01b/src/js/day01b.js
+++ day-01b/src/js/day01b.js
@@ -1,43 +1,53 @@
 const fs = require('fs');
 
-fs.readFile('input/input.txt', 'utf8', (error, data) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
+const read_file = async (filePath) => {
+  try {
+    const fileContents = await fs.promises.readFile(filePath, 'utf8');
+    return fileContents;
+  } catch (error) {
+    console.error(`Error reading file: ${error}`);
+    process.exit(1);
   }
-  calorie_counter(data); // Output: Contents of the mock.txt file
-});
+};
 
-
-function calorie_counter(input){
+const calorie_counter = async(filePath) =>{
   // split input into array of elf calorie counts
-  const calories = input.split(/\r\n/)
-  let elves_bags =  [0]; 
+  
+  let elves_bags = [0];
   let current_elf = 0;
   let top_3_elves = [0]//[0,0,0];//Array.from({ length: 3 }, () => 0);
+  let input = await read_file(filePath);
+  const calories = input.split(/\r\n/)
 
   calories.forEach(element => {
     // get each elf's calorie count
-    if(element==""){
+    if (element == "") {
       current_elf++;
-      elves_bags[current_elf]=0;
-    }else{
-      elves_bags[current_elf]+=parseInt(element);
+      elves_bags[current_elf] = 0;
+    } else {
+      elves_bags[current_elf] += parseInt(element);
     }
     // if the current elf's bag is bigger than one of the top 3, add it to the top 3
-    for(let i=0;i<3;i++){
-      if(elves_bags[current_elf]>top_3_elves[i]){
-        top_3_elves.splice(i,0,elves_bags[current_elf]);
+    for (let i = 0; i < 3; i++) {
+      if (elves_bags[current_elf] > top_3_elves[i]) {
+        top_3_elves.splice(i, 0, elves_bags[current_elf]);
         return;
       }
     }
-    
   });
   // Remove all values but the top 3
   top_3_elves.splice(3);
-  
-  // Give the answer
-  console.table(top_3_elves);
-  let total_calories = top_3_elves.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-  console.log(`Top 3 Elves total calories: ${total_calories}`);
+
+  return top_3_elves;
 }
+
+
+// Give the answer
+const filePath = 'input/input.txt';
+calorie_counter(filePath).then(result => { 
+  console.table(result);
+  let total_calories = result.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(`Top 3 Elves total calories: ${total_calories}`);
+});
+
+module.exports = calorie_counter;
